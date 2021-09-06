0ee250169bb98e414b8f5e561620c8c6ee36965b
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-09-06 17:34:28 +0500

    adding user component and a reuseable card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AddUser from './components/Users/AddUser';
 
 function App() {
   return (
     <div className="App">
-      
+      <AddUser />
     </div>
   );
 }
