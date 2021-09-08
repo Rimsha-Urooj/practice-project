5086e416593d1c12a4c5fb71ef121eb162905543
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-09-08 10:44:22 +0500

    adding validations

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -10,8 +10,18 @@ function AddUser(props) {
 
     const addUserHandler = (event) =>{
         event.preventDefault();
-        console.log(enteredUsername);
-        console.log(enteredAge);
+        
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+            return;
+        }
+
+        if (+enteredAge < 1){
+            return;
+        }
+        console.log(enteredUsername, enteredAge);
+      
+        setEnteredAge('');
+        setEnteredUsername('');
     }
 
     const usernameChangeHandler = (event) => {
@@ -24,10 +34,10 @@ function AddUser(props) {
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">User Name</label>
-                <input id="username" type="text" onChange={usernameChangeHandler} />
+                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
 
                 <label htmlFor="age"  >Age (Years)</label>
-                <input id="age" type="number" onChange={ageChangeHandler} />
+                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
                 <Button type="submit"> Add User</Button>
             </form>
         </Card>
