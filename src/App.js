da7e7c17c941084fa86de9e1a656c33adfbdd58b
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-09-08 10:58:33 +0500

    adding user list component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
   return (
     <div className="App">
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
