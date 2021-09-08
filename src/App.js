0b96426a8fd40fccc0aa570b4eb31e13c4f65fae
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-09-08 11:10:37 +0500

    managing a list of userState via stata

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
+
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+      setUsersList((prevUserList)=>{
+          return [...prevUserList, {name: uName, age: uAge, id:Math.random().toString()}]
+      })
+  }
+
   return (
     <div className="App">
-      <AddUser />
-      <UsersList users={[]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList  users={usersList} />
     </div>
   );
 }
